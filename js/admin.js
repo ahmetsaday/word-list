a5f880bacc80989533a449e2b63ff23fdcb7be63
ahmet saday <ahmetsaday34@gmail.com>
2020-06-01 13:24:31 +0300

    *word search and update

--- js/admin.js
+++ js/admin.js
@@ -1,36 +1,102 @@
+// hide the update form when the site is loaded.
+$( document ).ready(function() {
+    $("#update-area").hide()
+});
+
 // submit the new word to the database
-$("form").submit(function(e){
+$("#addNewWordForm").submit(function(e){
     e.preventDefault()
 
     var newWord = {
         id: new Date().getTime(),
-        word: "",
-        example: "",
-        description: "",
-        meaning: "",
+        word: $("#word-input").val(),
+        example: $("#example-input").val(),
+        description: $("#description-input").val(),
+        meaning: $("#meaning-input").val(),
         correctScore: 0,
         wrongScore: 0,
         active: true
     }
-    
-    newWord.word = $("#word-input").val()
-    newWord.example = $("#example-input").val()
-    newWord.description = $("#description-input").val()
-    newWord.meaning = $("#meaning-input").val()
 
     db.collection("words").add(newWord)
     .then(function(docRef) {
         //console.log("Document written with ID: ", docRef.id);
         $('input').val("");
         $('#submit').val("Add");
-        $("#info").html("The word has created successfully.")
+        $(".add-info").html("The word has created successfully.")
     })
     .catch(function(error) {
         $('input').val("");
         $('#submit').val("Add");
-        $("#info").html(error)
+        $(".add-info").html(error)
     });
 });
+
+
+// Global Document Id
+var docId = ""
+
+// Search the word in the database
+$("#searchWordForm").submit(function(e){
+    e.preventDefault()
+    
+    var queryWord = $("#search-input").val()
+    $(".search-info").html("")
+    $(".update-info").html("")
+
+    // Query
+    db.collection("words").where("word", "==", queryWord)
+    .get()
+    .then(function(querySnapshot) {
+
+        if (querySnapshot.empty) {
+            $("#update-area").fadeOut()            
+            $(".search-info").html("The word is not found.")
+          } else {
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                // console.log(doc.id, " => ", doc.data());
+                    
+                
+
+                // appear the update form
+                $("#update-area").fadeIn()
+                // write values to the inputs
+                $("#update-word-input").val(doc.data().word)
+                $("#update-description-input").val(doc.data().description)
+                $("#update-example-input").val(doc.data().example)
+                $("#update-meaning-input").val(doc.data().meaning)   
+                
+                // Write the doc id to the global doc id
+                docId = doc.id          
+            })
+          }
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+        $(".search-info").html("There is something wrong.")
+    })
+})
+
+// Update the word in the database
+$("#updateWordForm").submit(function(e){
+    e.preventDefault()
+
+    db.collection("words").doc(docId).update({
+        word: $("#update-word-input").val() ,
+        description: $("#update-description-input").val() ,
+        example: $("#update-example-input").val() ,
+        meaning: $("#update-meaning-input").val() 
+    })
+    .then(function() {
+        $(".update-info").html("Document successfully updated!")
+    })
+    .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+        $(".update-info").html("There is something wrong.")
+    })
+})
 
 // mix trigger for word list
 $("#logout").click(function(){
@@ -44,6 +110,7 @@ $("#logout").click(function(){
     });
 })
 
+// BackUp Data
 var totalWordArray = []
 var exportName = "backup"
 
@@ -64,6 +131,7 @@ function getData(){
     );
 }
 
+// backup to the words
 $("#download").click(function(){
     getData()
 })
